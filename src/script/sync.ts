f7d4ce527aff3c7d1323f92a63da5b82fd62cfe4
Austin Chen <akrolsmir@gmail.com>
2024-10-25 16:14:34 -0700

    Backup codegen/

--- src/script/sync.ts
+++ src/script/sync.ts
@@ -3,47 +3,39 @@ import { $ } from 'bun'
 // Commands to sync the local codegen folder with the fly and render volumes
 
 async function pullFly() {
-  try {
-    await $`rm -rf ./codegen && mkdir -p ./codegen`
-    await $`fly ssh console -C 'tar -czf /tmp/codegen_backup.tar.gz -C /app/codegen .'`
-    await $`fly sftp get /tmp/codegen_backup.tar.gz ./local_codegen_backup.tar.gz`
-    await $`tar -xzf ./local_codegen_backup.tar.gz -C ./codegen`
-    await $`rm ./local_codegen_backup.tar.gz`
-    console.log('Fly volume pulled successfully')
-  } catch (error) {
-    console.error('Error pulling Fly volume:', error)
-  }
+  await $`rm -rf ./codegen && mkdir -p ./codegen`
+  await $`fly ssh console -C 'tar -czf /tmp/codegen_backup.tar.gz -C /app/codegen .'`
+  await $`fly sftp get /tmp/codegen_backup.tar.gz ./local_codegen_backup.tar.gz`
+  await $`tar -xzf ./local_codegen_backup.tar.gz -C ./codegen`
+  await $`rm ./local_codegen_backup.tar.gz`
+  console.log('Fly volume pulled successfully')
 }
 
 async function pushFly() {
-  try {
-    await $`tar -czf ./codegen_backup.tar.gz -C ./codegen .`
-    await $`expect -c 'spawn fly ssh sftp shell; expect "»"; send "put ./codegen_backup.tar.gz /tmp/codegen_backup.tar.gz\r"; expect "»"; send "\x03"; expect eof'`
-    await $`expect -c 'spawn fly ssh console; expect "#"; send "find /app/codegen -mindepth 1 -delete\r"; expect "#"; send "tar -xzf /tmp/codegen_backup.tar.gz -C /app/codegen\r"; expect "#"; send "rm /tmp/codegen_backup.tar.gz\r"; expect "#"; send "exit\r"; expect eof'`
-    await $`rm ./codegen_backup.tar.gz`
-    console.log('Fly volume pushed successfully')
-  } catch (error) {
-    console.error('Error pushing Fly volume:', error)
-  }
+  await $`tar -czf ./codegen_backup.tar.gz -C ./codegen .`
+  await $`expect -c 'spawn fly ssh sftp shell; expect "»"; send "put ./codegen_backup.tar.gz /tmp/codegen_backup.tar.gz\r"; expect "»"; send "\x03"; expect eof'`
+  await $`expect -c 'spawn fly ssh console; expect "#"; send "find /app/codegen -mindepth 1 -delete\r"; expect "#"; send "tar -xzf /tmp/codegen_backup.tar.gz -C /app/codegen\r"; expect "#"; send "rm /tmp/codegen_backup.tar.gz\r"; expect "#"; send "exit\r"; expect eof'`
+  await $`rm ./codegen_backup.tar.gz`
+  console.log('Fly volume pushed successfully')
 }
 
 async function pullRender() {
-  try {
-    await $`rm -rf ./codegen && mkdir -p ./codegen`
-    await $`ssh srv-cq9kk73v2p9s73ck9jkg@ssh.oregon.render.com 'tar -czf - -C /app/codegen .' | tar -xzf - -C ./codegen`
-    console.log('Render volume pulled successfully')
-  } catch (error) {
-    console.error('Error pulling Render volume:', error)
-  }
+  await $`rm -rf ./codegen && mkdir -p ./codegen`
+  await $`ssh srv-cq9kk73v2p9s73ck9jkg@ssh.oregon.render.com 'tar -czf - -C /app/codegen .' | tar -xzf - -C ./codegen`
+  console.log('Render volume pulled successfully')
 }
 
 async function pushRender() {
-  try {
-    await $`tar -czf - -C ./codegen . | ssh srv-cq9kk73v2p9s73ck9jkg@ssh.oregon.render.com 'rm -rf /app/codegen/* && tar -xzf - -C /app/codegen'`
-    console.log('Render volume pushed successfully')
-  } catch (error) {
-    console.error('Error pushing Render volume:', error)
-  }
+  await $`tar -czf - -C ./codegen . | ssh srv-cq9kk73v2p9s73ck9jkg@ssh.oregon.render.com 'rm -rf /app/codegen/* && tar -xzf - -C /app/codegen'`
+  console.log('Render volume pushed successfully')
+}
+
+// Just bundle codegen into a tar. Name it codegen_<timestamp>.tar.gz
+async function backup() {
+  const timestamp = new Date().toISOString().slice(0, 16)
+  const filename = `./backup/codegen_${timestamp}.tar.gz`
+  await $`tar -czf ${filename} -C ./codegen .`
+  console.log(`Codegen backed up to ${filename}`)
 }
 
 async function pushAll() {
@@ -59,6 +51,7 @@ if (import.meta.main) {
     'push:fly': pushFly,
     'push:render': pushRender,
     'push:all': pushAll,
+    backup,
   }
 
   if (!(command in commands)) {
